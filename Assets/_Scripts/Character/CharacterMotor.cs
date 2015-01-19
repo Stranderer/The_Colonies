f0e5035003f4e4371ea61e0248a10f1807e0a483
Stranderer <ajoerg@projekt-u.ch>
2015-01-19 22:59:21 +0100

    Adding Moddeling Project

--- Assets/_Scripts/Character/CharacterMotor.cs
+++ Assets/_Scripts/Character/CharacterMotor.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class CharacterMotor : MonoBehaviour {
 
+	public Text infoText;
+
 	// Use this for initialization
 	void Start () {
-	
+		infoText = transform.Find("GUICanvas/ActionInfo").gameObject.GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
@@ -15,9 +18,16 @@ public class CharacterMotor : MonoBehaviour {
 
 		RaycastHit hit;
 		Vector3 fwd = transform.FindChild("Main Camera").transform.forward;
-		if (Physics.Raycast(transform.FindChild("Main Camera").transform.position, fwd, out hit, 10)){
+		if (Physics.Raycast(transform.FindChild("Main Camera").transform.position, fwd, out hit, 5)){
 			Debug.Log("There is something in front of the object!");
 			Debug.Log(hit.collider.transform.name);
+
+
+			infoText.text = "F";
+			infoText.color = new Color(255,255,255,1);
+
+		}else{
+			infoText.color = new Color(255, 255, 255, 0);
 		}
 	}
 }
